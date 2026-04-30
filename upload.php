d71465b29aa82c06fd18ce7b66a99e64dcd9cc95
Jennifer Nguyen <120598876+JenniferN654@users.noreply.github.com>
2026-04-30 11:14:11 -0500

    deleted the link that connect to style.css

--- upload.php
+++ upload.php
@@ -75,7 +75,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload Art - Web Gallz</title>
-    <link href="styles.css" type="text/css" rel="stylesheet">
+
     <!--separate upload css file for now -->
     <link href="upload.css" type="text/css" rel="stylesheet">
 </head>
